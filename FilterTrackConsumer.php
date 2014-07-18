08b5293fb9ec8a28ff06ae59b022411d4cdd170c
Martins Sipenko <martins.sipenko@accenture.com>
2014-07-18 11:31:07 +0000

    split class into own file

---
+++ FilterTrackConsumer.php
@@ -0,0 +1,35 @@
+<?php
+
+class FilterTrackConsumer extends OauthPhirehose
+{
+    /**
+    * Enqueue each status
+    *
+    * @param string $status
+    */
+    public function enqueueStatus($status)
+    {
+        $data = json_decode($status, true);
+        if (is_array($data) && isset($data['entities']['media'])) {
+            foreach ($data['entities']['media'] as $media) {
+                if ($media['type'] == 'photo') {
+                    $msg_body = json_encode($data);
+                    $msg = new AMQPMessage($msg_body, array('content_type' => 'text/plain', 'delivery_mode' => 2));
+                    $amqp->basic_publish($msg, $this->exchange);
+
+                    error_log($msg_body);
+                }
+            }
+        }
+    }
+
+    /**
+     * Set AMQP channel and exchange name
+     * @param PhpAmqpLib\Channel\AbstractChannel $amqp     AbstractChannel instance
+     * @param string                             $exchange Name of exnahnge
+     */
+    public function setAmqpAndExchange($amqp, $exchange) {
+        $this->amqp     = $amqp;
+        $this->exchange = $exchange;
+    }
+}
--- app.php
+++ app.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'vendor/autoload.php';
+require_once 'FilterTrackConsumer.php';
 
 use PhpAmqpLib\Message\AMQPMessage;
 use PhpAmqpLib\Connection\AMQPConnection;
@@ -37,37 +38,3 @@ $sc->consume(true);
 
 $ch->close();
 $conn->close();
-
-class FilterTrackConsumer extends OauthPhirehose
-{
-    /**
-    * Enqueue each status
-    *
-    * @param string $status
-    */
-    public function enqueueStatus($status)
-    {
-        $data = json_decode($status, true);
-        if (is_array($data) && isset($data['entities']['media'])) {
-            foreach ($data['entities']['media'] as $media) {
-                if ($media['type'] == 'photo') {
-                    $msg_body = json_encode($data);
-                    $msg = new AMQPMessage($msg_body, array('content_type' => 'text/plain', 'delivery_mode' => 2));
-                    $amqp->basic_publish($msg, $this->exchange);
-
-                    error_log($msg_body);
-                }
-            }
-        }
-    }
-
-    /**
-     * Set AMQP channel and exchange name
-     * @param PhpAmqpLib\Channel\AbstractChannel $amqp     AbstractChannel instance
-     * @param string                             $exchange Name of exnahnge
-     */
-    public function setAmqpAndExchange($amqp, $exchange) {
-        $this->amqp     = $amqp;
-        $this->exchange = $exchange;
-    }
-}
